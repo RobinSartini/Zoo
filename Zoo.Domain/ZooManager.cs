1e1d64762d0f5d5c0fc6dc1784f13d969718e292
malXcom <malcom.nouali@ynov.com>
2026-06-10 13:43:53 +0200

    refactor(REQ-Z-009): GetRation public accessibility updated to private

--- Zoo.Domain/ZooManager.cs
+++ Zoo.Domain/ZooManager.cs
@@ -24,7 +24,7 @@ public class ZooManager
     public int TotalAnimals => _animals.Count;
     public int TotalCapacityUsed => _animals.Sum(a => a.Value.Status == HealthStatus.Critical ? 2 : 1);
 
-    public double GetRationByCategory(AnimalCategory category) => Convert.ToDouble(category);
+    private double GetRationByCategory(AnimalCategory category) => Convert.ToDouble(category);
     
     public double CalculateDailyRation(int animalId)
     {
